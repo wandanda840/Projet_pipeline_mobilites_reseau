8b4d8f1a3094724c59d689b9be3ccee471e11909
wnanda <Wanda.Nanda@insa-renens.fr>
2026-09-02 20:08:58 +0200

    lecture de quelques informations supplémentaires sur les données récupérées

--- src/ingestion/position_vehicule.py
+++ src/ingestion/position_vehicule.py
@@ -19,13 +19,38 @@ from google.transit import gtfs_realtime_pb2  #on importe le module gtfs-realtim
 headers = {
     "User-Agent": "Projet Pipeline Mobilités Réseau - Étudiant"
 }
-def get_vehicle_positions():
+def get_vehicle_positions(): # cette fonction retourne un objet FeedMessage contenant les positions des véhicules
     url = "https://proxy.transport.data.gouv.fr/resource/star-rennes-integration-gtfs-rt-vehicle-position"
     try:
         response = requests.get(url, timeout=5, headers=headers)  # Timeout de 5 secondes
         response.raise_for_status()  # Vérifie si la requête a réussi
-        feed = gtfs_realtime_pb2.FeedMessage() # Initialisation de l'objet FeedMessage
-        feed.ParseFromString(response.content) # Parse le contenu binaire de la réponse dans l'objet FeedMessage
+        
+        #J'affiche le content type le content lenght et le content encoding pour vérifier que le flux est bien en binaire
+        print(f"Content-Type: {response.headers.get('Content-Type')}")
+        print(f"Content-Length: {response.headers.get('Content-Length')}")
+        print(f"Content-Encoding: {response.headers.get('Content-Encoding')}")
+        
+        
+        # #Je vérifie les entetes HTTP de validation de contenu client serveur
+        
+        # #j'affhiche aussi le LastMofified pour savoir quand le flux a été mis à jour pour la dernière fois
+        # print(f"Last-Modified: {response.headers.get('Last-Modified')}")
+        
+        # #Etag
+        # print(f"Etag: {response.headers.get('Etag')}")
+        
+        # #cache control
+        # print(f"Cache-Control: {response.headers.get('Cache-Control')}")
+        
+        
+        #je verifie ce qui se passe quand je lis la reponse comme du texte
+        #print(f"Response text: {response.text[:100]}")  # Affiche les 100 premiers caractères du texte de la réponse
+        #effectivement c'est illisible car la réponse est un binaire
+        
+        
+        
+        feed = gtfs_realtime_pb2.FeedMessage() # initialisation de l'objet FeedMessage
+        feed.ParseFromString(response.content) # je mets le contenu binaire de la réponse dans l'objet FeedMessage
         return feed
     except requests.exceptions.RequestException as e:
         print(f"Erreur lors de l'appel à l'API : {e}")
@@ -63,7 +88,10 @@ if __name__ == "__main__":
             df = feed_to_dataframe(feed)
             # Ajouter un horodatage pour savoir quand les données ont été récupérées
             df['timestamp'] = datetime.now()
-            print(df.head())  # Affiche les premières lignes du DataFrame
+            print(df.head(20))  # Affiche les premières lignes du DataFrame
+            
+            #j'afficche le nombre de véhicules récupérés
+            print(f"Nombre de véhicules récupérés : {len(df)}")
         else:
             print("Aucune donnée récupérée.")
 
